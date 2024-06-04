6f2ca82d35c51b198d786e050870aa18a912f162
Filipp Vorobev <philv@mail.ru>
2024-06-04 20:14:26 +0300

    more optimaze

--- src/test/java/QAGuruHW3.java
+++ src/test/java/QAGuruHW3.java
@@ -1,55 +1,75 @@
 import com.codeborne.selenide.Configuration;
+
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.byValue;
 import static com.codeborne.selenide.Selenide.*;
 
 
 public class QAGuruHW3 {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.browserSize = "1900x1080";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+
     @Test
     void fillFormTest() {
-        Configuration.pageLoadStrategy = "eager";
-        open("https://demoqa.com/automation-practice-form");
+        open("/automation-practice-form");
 
-        $("[id=firstName]").setValue("Filipp");
-        $("[id=lastName]").setValue("Vorobev");
-        $("[id=userEmail]").setValue("randomMail@mailemail.com");
+        $("#firstName").setValue("Filipp");
+        $("#lastName").setValue("Vorobev");
+        $("#userEmail").setValue("randomMail@email.com");
 
-        SelenideElement gender = $("[id=gender-radio-3]");
-        actions().moveToElement(gender).click(gender).perform();
+        $("#genterWrapper").$(byText("Male")).click();
 
-        $("[id=userNumber]").setValue("9213824198");
+       /* SelenideElement gender = $("[id=gender-radio-3]");
+        actions().moveToElement(gender).click(gender).perform();*/
 
-        $("[id=dateOfBirthInput]").click();
-        $("[class=react-datepicker__month-select]").selectOption(9);
-        $("[class=react-datepicker__year-select]").selectOption("1993");
-        $("[class=react-datepicker__week]").click();
+        $("#userNumber").setValue("1234567890");
+
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOption("October");
+        $(".react-datepicker__year-select").selectOption("1993");
+        $(".react-datepicker__day--011").click();
 
 
-        $("[id=subjectsInput]").setValue("a");
-        $("[id=react-select-2-option-0]").click();
+        $("#subjectsInput").setValue("a");
+        $("#subjectsWrapper").$(byText("Maths")).click();
 
-        SelenideElement hob1 = $("[id=hobbies-checkbox-1]");
+        $("#hobbiesWrapper").$(byText("Sports")).click();
+        $("#hobbiesWrapper").$(byText("Reading")).click();
+        $("#hobbiesWrapper").$(byText("Music")).click();
+        /*SelenideElement hob1 = $("[id=hobbies-checkbox-1]");
         SelenideElement hob2 = $("[id=hobbies-checkbox-2]");
         SelenideElement hob3 = $("[id=hobbies-checkbox-3]");
         actions().moveToElement(hob1).click(hob1).perform();
         actions().click(hob2).perform();
-        actions().click(hob3).perform();
+        actions().click(hob3).perform();*/
 
         File file = new File("D:\\2023-07-20 202614.png");
-        $("[id=uploadPicture]").uploadFile(file);
+        $("#uploadPicture").uploadFile(file);
 
-        $("[id=currentAddress]").setValue("some adress");
+        $("#currentAddress").setValue("some adress");
 
-        $("#stateCity-wrapper > div:nth-of-type(2) svg").click();
+        $("#state").click();
+        $(byText("NCR")).click();
+        $("#city").click();
+        $(byText("Delhi")).click();
+        /*$("#stateCity-wrapper > div:nth-of-type(2) svg").click();
         $("#react-select-3-option-0").click();
         $("div:nth-of-type(3) div.css-1hwfws3").click();
-        $("#react-select-4-option-0").click();
+        $("#react-select-4-option-0").click();*/
 
-        $("[id=submit]").scrollIntoView(true).click();
-        Selenide.sleep(10000);
+        $("#submit").scrollTo().click();
+        //Selenide.sleep(10000);
     }
 }
