df85523ab95e5a169eab9a63e74cdcd1d6695243
Filipp Vorobev <philv@mail.ru>
2024-07-29 21:16:16 +0300

    added new tasks, modified TestBase

--- src/test/java/DemoqaAutoformRemoteTests.java
+++ src/test/java/DemoqaAutoformRemoteTests.java
@@ -1,3 +1,4 @@
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
@@ -8,7 +9,7 @@ import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
 import static io.qameta.allure.Allure.step;
 
-public class DemoqaAutoformRemoteTests extends TestBase {
+public class DemoqaAutoformRemoteTests extends TestBase{
 
 
     @Test
--- src/test/java/TestBase.java
+++ src/test/java/TestBase.java
@@ -10,15 +10,14 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.util.Map;
 
-import static com.codeborne.selenide.Selenide.executeJavaScript;
 
 public class TestBase {
     @BeforeAll
     static void configuration() {
-        Configuration.baseUrl = System.getProperty("baseURL"); //"https://demoqa.com"
+        Configuration.baseUrl = System.getProperty("https://demoqa.com"); //
         Configuration.pageLoadStrategy = "eager";
-        Configuration.browser = System.getProperty("browser");
-        Configuration.browserVersion = System.getProperty("browserVersion");
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion", "125");
         Configuration.browserSize = System.getProperty("browserSize");
         Configuration.remote = System.getProperty("remoteURL"); // "https://user1:1234@selenoid.autotests.cloud/wd/hub"
 
@@ -31,10 +30,12 @@ public class TestBase {
 
 
     }
+
     @BeforeEach
-    void addListener(){
+    void addListener() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
+
     @AfterEach
     void addAttachments() {
         Attach.screenshotAs("Last screenshot");
